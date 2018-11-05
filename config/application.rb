7949ab43b3159042efee4bdcc5497a5f35de0482
stivan622 <stivan.tr@gmail.com>
2018-11-06 02:47:42 +0900

    set time zone

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module RailsPreset
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
   end
 end
